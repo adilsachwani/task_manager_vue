9fb48021f242b04334fd0f03dca9f20e8e1e1f1a
Adil Sachwani <adilsachwani>
2021-06-11 16:26:15 +0500

    initial code

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+    </header>
+</template>
+
+<script>
+
+import './Button.vue'
+import Button from './Button.vue'
+
+export default {
+    name: "Header",
+    props: ['title'],
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
